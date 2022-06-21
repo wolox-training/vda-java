b59f4b9fb35981e4c1f55150546e325555b841bb
victor <victor.dario.ayala@accenture.com>
2022-06-21 14:42:39 -0500

    Create bookRepository interface card https://trello.com/c/yNYUtYVk/15-crear-un-repositorio-de-books

---
+++ training/src/main/java/wolox/training/repositoies/BookRepository.java
@@ -0,0 +1,9 @@
+package wolox.training.repositoies;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import wolox.training.models.Book;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+    Book findFirstByAuthor(String author);
+}
