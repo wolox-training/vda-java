ff59fb3d3d84ad127a29526a930ff782f7ae984e
victor <victor.dario.ayala@accenture.com>
2022-06-28 10:19:24 -0500

    -Fixed with recomendations -merge and resolve conflicts card https://trello.com/c/2fqH75mu/19-crear-modelo-de-user

--- training/src/main/java/wolox/training/controllers/BookController.java
+++ training/src/main/java/wolox/training/controllers/BookController.java
@@ -1,7 +1,6 @@
 package wolox.training.controllers;
 
 import java.util.List;
-import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
